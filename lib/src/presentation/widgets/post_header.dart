4fca4213151e84399fd9e2622e2ea8275fba7cdf
Mohammad Shawky <33393194+midoshawky@users.noreply.github.com>
2026-04-08 16:51:38 +0200

    feat: add post interaction icons and implement post header menu with copy and report actions

--- lib/src/presentation/widgets/post_header.dart
+++ lib/src/presentation/widgets/post_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../models/user.dart';
 import 'user_avatar.dart';
@@ -54,22 +55,86 @@ class PostHeader extends StatelessWidget {
             ],
           ),
         ),
-        TextButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.add_circle, color: Color(0xFF4535C1), size: 16),
-          label: Text(
-            'Follow',
-            style: GoogleFonts.inter(
-              color: const Color(0xFF4535C1),
-              fontWeight: FontWeight.w600,
-              fontSize: 14,
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add_rounded,
+                color: Color(0xFF4535C1),
+                size: 20,
+              ),
+              label: Text(
+                'Follow',
+                style: GoogleFonts.inter(
+                  color: const Color(0xFF4535C1),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                minimumSize: Size.zero,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
             ),
-          ),
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
-            minimumSize: Size.zero,
-            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          ),
+            PopupMenuButton<String>(
+              icon: const Icon(Icons.more_horiz, color: Color(0xFF787878)),
+              padding: EdgeInsets.zero,
+              position: PopupMenuPosition.under,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              onSelected: (value) {
+                // TODO: Handle copy link and report
+              },
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  value: 'copy',
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/copy.svg',
+                        package: 'feed_module',
+                        width: 20,
+                        height: 20,
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        'Copy link',
+                        style: GoogleFonts.inter(
+                          fontSize: 14,
+                          color: const Color(0xFF1F1F1F),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                PopupMenuItem(
+                  value: 'report',
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/report.svg',
+                        package: 'feed_module',
+                        width: 20,
+                        height: 20,
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        'Report',
+                        style: GoogleFonts.inter(
+                          fontSize: 14,
+                          color: const Color(0xFF1F1F1F),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ],
     );
